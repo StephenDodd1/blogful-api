1cf8bc3c4e8244d84d7c6ef844d38dee19dfb8ec
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-07-08 13:31:00 -0500

    added endpoint and tests currently not passing/ working to fix it

---
+++ test/articles-endpoints.spec.js
@@ -0,0 +1,64 @@
+const { expect } = require('chai')
+const knex = require('knex')
+const app = require('../src/app')
+
+describe.only('Atricles Endpoints tests', function() {
+   let db
+
+   before('make new knex instance', () => {
+      db = knex({
+         client: 'pg',
+         connection: process.env.TEST_DB_URL,
+      })
+      app.set('db', db)
+   })
+
+   after('disconnect from db', () => db.destroy())
+
+   before('clean the table', () => db('blogful_articles').truncate())
+
+   context('given there are articles in the table', () => {
+      const testArticles = [
+         {
+            id: 1,
+            title: 'First test post!',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?',
+            date_published: '2019-01-01T16:2832.615Z',
+            style: 'How-to',
+         },
+         {
+            id: 2,
+            date_published: '2000-05-22T16:28:32.615Z',
+            title: 'Second test post!',
+            style: 'News',
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, exercitationem cupiditate dignissimos est perspiciatis, nobis commodi alias saepe atque facilis labore sequi deleniti. Sint, adipisci facere! Velit temporibus debitis rerum.'
+         },
+         {
+            id: 3,
+            date_published: '1919-12-22T16:28:32.615Z',
+            title: 'Third test post!',
+            style: 'Listicle',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, voluptate? Necessitatibus, reiciendis? Cupiditate totam laborum esse animi ratione ipsa dignissimos laboriosam eos similique cumque. Est nostrum esse porro id quaerat.'
+         },
+         {
+            id: 4,
+            date_published: '1919-12-22T16:28:32.615Z',
+            title: 'Fourth test post!',
+            style: 'Story',
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum molestiae accusamus veniam consectetur tempora, corporis obcaecati ad nisi asperiores tenetur, autem magnam. Iste, architecto obcaecati tenetur quidem voluptatum ipsa quam?'
+         }
+      ];
+
+      beforeEach('insert articles', () => {
+         return db
+         .into('blogful_articles')
+         .insert(testArticles)
+      })
+
+      it('GET /articles responds with 200 and all of the articles', () => {
+         return supertest(app)
+            .get('/articles')
+            .expect(200, testArticles)
+      })
+   })
+})
